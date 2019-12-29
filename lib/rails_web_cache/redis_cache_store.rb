7e8701bf5dbde3ac521206bf66f07c0f0b4f5658
Boris Bresciani <boris2bresciani@gmail.com>
2019-12-29 22:27:36 +0100

    Fix entry redis

--- lib/rails_web_cache/redis_cache_store.rb
+++ lib/rails_web_cache/redis_cache_store.rb
@@ -14,10 +14,22 @@ module RailsWebCache
       redis.scan_each(match: "*#{query.downcase}*").to_a if query
     end
 
+    def entry(key, options = {})
+      return nil unless key
+      return nil if type(key) == 'zset'
+      entry = read_entry(key, options)
+      return nil unless entry
+      RailsWebCache::Entry.new(entry)
+    end
+
     private
 
     def redis
       @redis ||= cache.redis
+    end
+
+    def type(key)
+      redis.type(key)
     end
   end
 end
