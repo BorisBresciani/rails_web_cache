c9a2386fd111f26d09c2491317bc601994d82c2b
Benjamin Darcet <b.darcet@gmail.com>
2019-12-31 18:30:49 +0100

    add disabled attribute to prevent user to click on the destroy all button

--- app/assets/javascripts/rails_web_cache/keys.js
+++ app/assets/javascripts/rails_web_cache/keys.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     $('#select-all-keys').on('click', function() {
         var checked = $(this).is(':checked');
         if (checked) {
-            $('#delete-all-keys').removeClass('disabled');
+            $('#delete-all-keys').removeClass('disabled').prop('disabled', false);
         } else {
-            $('#delete-all-keys').addClass('disabled');
+            $('#delete-all-keys').addClass('disabled').prop('disabled', true);
         }
         $("input[name='keys[]']").each(function() {
             $(this).prop('checked', checked);
@@ -14,9 +14,9 @@ $(document).ready(function() {
         var checked = $("input:checked[name='keys[]']");
         var size = checked.length;
         if (size > 0) {
-            $('#delete-all-keys').removeClass('disabled');
+            $('#delete-all-keys').removeClass('disabled').prop('disabled', false);
         } else {
-            $('#delete-all-keys').addClass('disabled');
+            $('#delete-all-keys').addClass('disabled').prop('disabled', true);
         }
     });
 });
